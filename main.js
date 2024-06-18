057c07ad6f283b6350bbd90e3b5a7188626a1c6d
Jéssica Nascimento <160242497+profajessica12@users.noreply.github.com>
2024-06-18 11:49:37 -0300

    pode

--- main.js
+++ main.js
@@ -13,8 +13,10 @@ for(let i=0;i <botoes.length;i++){
         function calculaTempo(tempoObjetivo) {
             let tempoAtual = new Date();
             let tempoFinal = tempoObjetivo - tempoAtual;
-            let segundos = tempoFinal / 1000;
+            let segundos = Math.floor(tempoFinal / 1000);
             let minutos = Math.floor(segundos / 60);
+            let horas = Math.floor(minutos / 60);
             let dias = Math.floor(horas / 24);
             return dias + “ dias “ + horas + “ horas “ + minutos + “
-minutos “ + segundos + “ segundos”;}
+            minutos “ + segundos + “ segundos”;
+            }

d6e4e7acfc54337ce075a6eb96d03d7496f7e38d
Jéssica Nascimento <160242497+profajessica12@users.noreply.github.com>
2024-06-18 11:52:45 -0300

    horas

--- main.js
+++ main.js
@@ -17,6 +17,9 @@ for(let i=0;i <botoes.length;i++){
             let minutos = Math.floor(segundos / 60);
             let horas = Math.floor(minutos / 60);
             let dias = Math.floor(horas / 24);
+            segundos %= 60;
+            minutos %= 60;
+            horas %= 24;
             return dias + “ dias “ + horas + “ horas “ + minutos + “
             minutos “ + segundos + “ segundos”;
             }
